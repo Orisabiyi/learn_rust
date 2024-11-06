39adf718bb1bf588d86ea21d09436cc6fb1a7600
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-06 16:55:25 +0100

    feat: implement user guess input and comparison with secret number in guessing game

--- rust/projects/guessing_game/src/main.rs
+++ rust/projects/guessing_game/src/main.rs
@@ -1,23 +1,32 @@
 // import standard input and output
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
     
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {secret_number}")
-    
-    println!("Please input your guess.");
-
     // declaring a mutable variable
     let mut guess = String::new();
+
+    // generating random number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Please input your guess.");
+
 
     io::stdin()
     .read_line(&mut guess)
     .expect("Faild to read line");
 
-    println!("You guessed: {}", guess.trim());
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    // comparing
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
-    
